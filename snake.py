e3b6799f45b4f93c6ca50d8ad3071d296903c027
brayan <brayan3288@gmail.com>
2020-03-22 23:48:57 -0500

    Function of movemer, ok

--- snake.py
+++ snake.py
@@ -1,4 +1,7 @@
 import turtle
+import time
+
+posponer = 0.2
 
 # Escenario del juego
 window = turtle.Screen()
@@ -13,9 +16,26 @@ head.shape("square")
 head.color("green")
 head.penup()
 head.goto(0,0)
-head.direction = "stop"
+head.direction = "down"
+
+# Funciones
+def mov():
+    if head.direction == "up":
+        y = head.ycor()
+        head.sety(y + 20)
+    if head.direction == "down":
+        y = head.ycor()
+        head.sety(y - 20)
+    if head.direction == "right":
+        x = head.xcor()
+        head.setx(x + 20)
+    if head.direction == "left":
+        x = head.xcor()
+        head.setx(x - 20)
 
 while True:
     window.update()
+    mov()
+    time.sleep(posponer)
 
 window.exitonclick() 
