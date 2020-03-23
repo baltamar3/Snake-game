507d8518807b52c7de1451492c3f65ca087bae5b
brayan <brayan3288@gmail.com>
2020-03-22 23:26:17 -0500

    escenario ok

---
+++ snake.py
@@ -0,0 +1,9 @@
+import turtle
+
+# Escenario del juego
+window = turtle.Screen()
+window.title("Snake Game @BrayanAltamar")
+window.bgcolor("black")
+window.setup(width=600,height=600)
+
+window.exitonclick() 
