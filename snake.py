47b36cc5ae291b3a7a5c6f49b58d0a1ff492c99e
brayan <brayan3288@gmail.com>
2020-03-22 23:39:47 -0500

    Snake head ok while True:

--- snake.py
+++ snake.py
@@ -6,4 +6,16 @@ window.title("Snake Game @BrayanAltamar")
 window.bgcolor("black")
 window.setup(width=600,height=600)
 
+# Cabeza Serpiente
+head = turtle.Turtle()
+head.speed(0)
+head.shape("square")
+head.color("green")
+head.penup()
+head.goto(0,0)
+head.direction = "stop"
+
+while True:
+    window.update()
+
 window.exitonclick() 
